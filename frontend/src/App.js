0f7636f929c70de041f0b2ec3017bdf52ef80a03
Igor Trindade <igorhideki@gmail.com>
2019-12-07 16:32:13 -0400

    Add react-toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import Routes from './routes';
 import history from './services/history';
@@ -10,6 +11,7 @@ function App() {
     <Router history={history}>
       <Routes />
       <GlobalStyles />
+      <ToastContainer autoClose={3000} />
     </Router>
   );
 }
