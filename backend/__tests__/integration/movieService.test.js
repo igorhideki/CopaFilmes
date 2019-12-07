e5ae603434b0ff3e2ffa9626a0b74d874460f428
Igor Trindade <igorhideki@gmail.com>
2019-12-07 12:08:12 -0400

    Add create champioship structure on movieService

--- backend/__tests__/integration/movieService.test.js
+++ backend/__tests__/integration/movieService.test.js
@@ -2,6 +2,7 @@ import MovieService from '../../src/app/services/MovieService';
 
 import movies from '../mocks/movies';
 import moviesSorted from '../mocks/moviesSorted';
+import championshipStructure from '../mocks/championshipStructure';
 
 describe('ChampionshipService', () => {
   const movieService = new MovieService();
@@ -10,5 +11,11 @@ describe('ChampionshipService', () => {
     const result = movieService.sortMovieList(movies);
 
     expect(result).toEqual(moviesSorted);
+  });
+
+  it('should return create championship structure', async () => {
+    const result = movieService.createChampionshipStructure(moviesSorted);
+
+    expect(result).toEqual(championshipStructure);
   });
 });
--- backend/src/app/services/MovieService.js
+++ backend/src/app/services/MovieService.js
@@ -5,13 +5,20 @@ export default class MovieService {
         return -1;
       }
 
-      if (movieA.titulo === movieB.titulo) {
-        return 0;
-      }
-
       return 1;
     });
 
     return moviesSorted;
+  }
+
+  createChampionshipStructure(movies) {
+    const structure = [];
+    const { length } = movies;
+
+    for (let i = 0; i < length / 2; i += 1) {
+      structure.push(movies[i], movies[length - 1 - i]);
+    }
+
+    return structure;
   }
 }
