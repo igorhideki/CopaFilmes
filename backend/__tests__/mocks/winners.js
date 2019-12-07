54a3c74d7b207d0e8567ba3c4638af942bb56125
Igor Trindade <igorhideki@gmail.com>
2019-12-07 12:47:28 -0400

    Add mock of winners

---
+++ backend/__tests__/mocks/winners.js
@@ -0,0 +1,14 @@
+export default {
+  winner: {
+    id: 'tt4154756',
+    titulo: 'Vingadores: Guerra Infinita',
+    ano: 2018,
+    nota: 8.8,
+  },
+  viceWinner: {
+    id: 'tt3606756',
+    titulo: 'Os Incríveis 2',
+    ano: 2018,
+    nota: 8.5,
+  },
+};
