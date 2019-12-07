4803e54f48717740bcfacd28298fd6dbe756af4f
Igor Trindade <igorhideki@gmail.com>
2019-12-07 11:12:51 -0400

    Update test name

--- backend/__tests__/integration/movie.test.js
+++ backend/__tests__/integration/movie.test.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import app from '../../src/app';
 
-describe('Movies', () => {
+describe('MovieController', () => {
   it('should return an array', async () => {
     const response = await request(app).get('/movies');
 
