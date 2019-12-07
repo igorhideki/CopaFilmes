05cb81a3541b3450a8e0cbfb00d73a94222278ef
Igor Trindade <igorhideki@gmail.com>
2019-12-07 12:48:01 -0400

    Add getChampionshipResult on movieService

--- backend/__tests__/integration/movieService.test.js
+++ backend/__tests__/integration/movieService.test.js
@@ -3,6 +3,7 @@ import MovieService from '../../src/app/services/MovieService';
 import movies from '../mocks/movies';
 import moviesSorted from '../mocks/moviesSorted';
 import championshipStructure from '../mocks/championshipStructure';
+import winners from '../mocks/winners';
 
 describe('ChampionshipService', () => {
   const movieService = new MovieService();
@@ -17,5 +18,29 @@ describe('ChampionshipService', () => {
     const result = movieService.createChampionshipStructure(moviesSorted);
 
     expect(result).toEqual(championshipStructure);
+  });
+
+  it('should compare two movies and returns the highest rated movie', () => {
+    const movieA = { nota: 2, titulo: 'movieA' };
+    const movieB = { nota: 9, titulo: 'movieB' };
+
+    const result = movieService.compareTwoMovies(movieA, movieB);
+
+    expect(result).toEqual(movieB);
+  });
+
+  it('should compare two movies with the same rate and returns the movie in alphabetical order', () => {
+    const movieA = { nota: 9, titulo: 'movieA' };
+    const movieB = { nota: 9, titulo: 'movieB' };
+
+    const result = movieService.compareTwoMovies(movieA, movieB);
+
+    expect(result).toEqual(movieA);
+  });
+
+  it('should return a object with winner and vice winner', () => {
+    const result = movieService.getChampionshipResult(championshipStructure);
+
+    expect(result).toEqual(winners);
   });
 });
--- backend/src/app/services/MovieService.js
+++ backend/src/app/services/MovieService.js
@@ -11,14 +11,45 @@ export default class MovieService {
     return moviesSorted;
   }
 
-  createChampionshipStructure(movies) {
+  createChampionshipStructure(moviesSorted) {
     const structure = [];
-    const { length } = movies;
+    const { length } = moviesSorted;
 
     for (let i = 0; i < length / 2; i += 1) {
-      structure.push(movies[i], movies[length - 1 - i]);
+      structure.push(moviesSorted[i], moviesSorted[length - 1 - i]);
     }
 
     return structure;
+  }
+
+  compareTwoMovies(movieA, movieB) {
+    if (movieA.nota === movieB.nota) {
+      const moviesSorted = this.sortMovieList([movieA, movieB]);
+
+      return moviesSorted[0];
+    }
+
+    return movieA.nota > movieB.nota ? movieA : movieB;
+  }
+
+  getChampionshipResult(structure) {
+    const { length } = structure;
+
+    if (length > 2) {
+      const currentStructure = [];
+
+      for (let i = 0; i < length - 1; i += 2) {
+        const movieA = structure[i];
+        const movieB = structure[i + 1];
+        currentStructure.push(this.compareTwoMovies(movieA, movieB));
+      }
+
+      return this.getChampionshipResult(currentStructure);
+    }
+
+    return {
+      winner: structure[0],
+      viceWinner: structure[1],
+    };
   }
 }
