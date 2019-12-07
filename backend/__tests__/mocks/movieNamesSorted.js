5441d65a94a120373ff36c83c4054b89039c1229
Igor Trindade <igorhideki@gmail.com>
2019-12-07 11:13:08 -0400

    Add mocks

---
+++ backend/__tests__/mocks/movieNamesSorted.js
@@ -0,0 +1,10 @@
+export default [
+  'Deadpool 2',
+  'Han Solo: Uma História Star Wars',
+  'Hereditário',
+  'Jurassic World: Reino Ameaçado',
+  'Oito Mulheres e um Segredo',
+  'Os Incríveis 2',
+  'Thor: Ragnarok',
+  'Vingadores: Guerra Infinita',
+];
