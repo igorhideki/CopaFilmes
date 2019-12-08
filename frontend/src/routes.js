e1cf9185a74a29e6d71963936c6b8bc79c3e5bcc
Igor Trindade <igorhideki@gmail.com>
2019-12-08 17:16:19 -0400

    Add result page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '~/pages/Home';
+import Result from '~/pages/Result';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" component={Home} exact />
+      <Route path="/resultado" component={Result} />
     </Switch>
   );
 }
